90eadc7d154dfb16d9d82a3b0ffd4fe2e1e0e47d
shanemmay <shanemarvinmay@hotmail.com>
2019-02-17 19:08:28 -0800

    Updates

---
+++ precache-manifest.158b63b4fe99e4900a0d32c6690ae0b8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "09839aee69eea11fb504",
+    "url": "/journal/static/css/main.713dcb98.chunk.css"
+  },
+  {
+    "revision": "09839aee69eea11fb504",
+    "url": "/journal/static/js/main.09839aee.chunk.js"
+  },
+  {
+    "revision": "f9d0b5b7273838050e4c",
+    "url": "/journal/static/js/runtime~main.f9d0b5b7.js"
+  },
+  {
+    "revision": "f9c8575e3ea9cefdd925",
+    "url": "/journal/static/css/2.8205e4eb.chunk.css"
+  },
+  {
+    "revision": "f9c8575e3ea9cefdd925",
+    "url": "/journal/static/js/2.f9c8575e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/journal/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/journal/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/journal/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/journal/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/journal/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "9a16b9cf89ed4e9827e96a4caf20b4b5",
+    "url": "/journal/index.html"
+  }
+];
